c570d48de05af577feccf70b1e3cbb0e8cd5dc49
Catherine Boileau <cboileau@ferlab.bio>
2020-08-04 16:30:39 -0400

    WIP Dictionary

--- scripts/dictionary/schemas/index.js
+++ scripts/dictionary/schemas/index.js
@@ -23,10 +23,8 @@ const donor = require('./donor.json');
 const specimen = require('./specimen.json');
 const primary_diagnosis = require('./primary_diagnosis.json');
 const treatment = require('./treatment.json');
-const chemotherapy = require('./chemotherapy.json');
-const hormone_therapy = require('./hormone_therapy.json');
-const radiation = require('./radiation.json');
 const follow_up = require('./follow_up.json');
+const exposure = require('./exposure.json');
 
 module.exports = [
   sample_registration,
@@ -34,8 +32,6 @@ module.exports = [
   specimen,
   primary_diagnosis,
   treatment,
-  chemotherapy,
-  hormone_therapy,
-  radiation,
   follow_up,
+  exposure
 ];
