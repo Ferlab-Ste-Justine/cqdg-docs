39a8abf4f8814424695950100d809c625db10162
YanLabCHU <ymoussaoui@ferlab.bio>
2020-08-18 14:43:39 -0400

    feature: Adds Sass support as a docusaurus plugin.

---
+++ website/sass-loader-plugin/index.js
@@ -0,0 +1,56 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = function (context, options) {
+    console.log('H: ' + context);
+    const devEnv = process.env.NODE_ENV === 'development';
+    return {
+        name: "sass-loader-plugin",
+        configureWebpack() {
+            return {
+                plugins: [
+                    new MiniCssExtractPlugin({
+                        filename: devEnv ? '[name].css' : '[name].[hash].css',
+                        chunkFilename: devEnv ? '[id].css' : '[id].[hash].css'
+                    })
+                ],
+                module: {
+                    rules: [
+                        {
+                            test: /\.module\.s(a|c)ss$/,
+                            loader: [
+                                devEnv ? 'style-loader' :
+                                    MiniCssExtractPlugin.loader,
+                                {
+                                    loader: 'css-loader',
+                                    options: {
+                                        modules: true,
+                                        sourceMap: devEnv
+                                    }
+                                },
+                                {
+                                    loader: 'sass-loader',
+                                    options: {
+                                        sourceMap: devEnv
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            test: /\.s(a|c)ss$/,
+                            exclude: /\.module.(s(a|c)ss)$/,
+                            loader: [
+                                devEnv ? 'style-loader' : MiniCssExtractPlugin.loader,
+                                'css-loader',
+                                {
+                                    loader: 'sass-loader',
+                                    options: {
+                                        sourceMap: devEnv
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+            }
+        }
+    }
+}
