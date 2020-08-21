8d04aaa1b1f7028b6417e9ee524afa384c126c1d
YanLabCHU <ymoussaoui@ferlab.bio>
2020-08-21 08:47:47 -0400

    feature: #49 Update Algolia configuration.

--- website/src/utils.js
+++ website/src/utils.js
@@ -23,4 +23,4 @@ const schemasAvailable = require('../static/data/schemas/schema-versions.json');
 export const getLatestVersion = () => schemasAvailable.sort().slice(-1)[0];
 
 export const isSearchAvailable =
-  !process.env.ALGOLIA_API_KEY || !process.env.ALGOLIA_INDEX ? false : true;
+  !process.env.ALGOLIA_API_KEY || !process.env.ALGOLIA_INDEX || !process.env.ALGOLIA_APP_ID ? false : true;
