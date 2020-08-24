041de31f05ec5cf79afd413797789c93c3fa15f7
YanLabCHU <ymoussaoui@ferlab.bio>
2020-08-24 10:38:00 -0400

    feature: #51 Adds Translation Provider

---
+++ website/src/theme/IntlProvider/index.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { IntlProvider } from 'react-intl';
+import config from './translations/'
+import TranslationContext from '../TranslationContext';
+let a = true;
+
+function I18NProvider({ children }) {
+    const { state } = useContext(TranslationContext);
+    const locale = state.locale;
+
+    return (
+        <IntlProvider messages={config[locale]} locale={locale} >
+            {children}
+        </IntlProvider>
+    )
+}
+
+export default I18NProvider;
