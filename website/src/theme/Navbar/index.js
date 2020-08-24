430d87f23104ed760f3e9b42fe09cecc3b6d1708
YanLabCHU <ymoussaoui@ferlab.bio>
2020-08-24 10:41:46 -0400

    feature: #51 Use translation in Navbar.

--- website/src/theme/Navbar/index.js
+++ website/src/theme/Navbar/index.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './header.module.scss';
 import OpenNew from '../../components/OpenNew';
+import { FormattedMessage } from 'react-intl';
+import t from '../Utils/translation'
+import TranslationContext from '../TranslationContext';
 
 function Navbar() {
     // By default, no link is active
     let [selectedIndex, setSelectedIndex] = useState(-1);
 
+    const { state, dispatch } = useContext(TranslationContext);
+
     const links = [{
-        title: 'Accès',
+        title: t('access'),
         action: () => { setSelectedIndex(0); }
     },
     {
-        title: 'Soumettre',
+        title: t('submit'),
         action: () => { setSelectedIndex(1); }
     },
     {
-        title: 'Dictionnaire',
+        title: t('dictionary'),
         action: () => { setSelectedIndex(2); }
     }];
+
+    const switchLanguage = () => {
+        dispatch({
+            type: "CHANGE_LOCALE",
+            locale: state.locale === 'fr' ? 'en' : 'fr'
+        });
+    }
 
     return (
         <div className={styles["theme-navbar"]}>
@@ -37,20 +49,24 @@ function Navbar() {
                 )}
                 <div className={styles["theme-navbar__links__divider"]}></div>
                 <div className={styles["theme-navbar__links__right-item"]}>
-                    <div className={styles["theme-navbar__links__right-item__text"]}>Portail</div>
+                    <div className={styles["theme-navbar__links__right-item__text"]}>
+                        {t('portal')}
+                    </div>
                     <OpenNew></OpenNew>
                 </div>
                 <div className={styles["theme-navbar__links__right-item"]}>
-                    <div className={styles["theme-navbar__links__right-item__text"]}>Site web</div>
+                    <div className={styles["theme-navbar__links__right-item__text"]}>
+                        {t('website')}
+                    </div>
                     <OpenNew></OpenNew>
                 </div>
                 <div className={styles["theme-navbar__links__lang"]}>
-                    <div className={styles["theme-navbar__links__lang__content"]}>
-                        EN
+                    <div onClick={switchLanguage} className={styles["theme-navbar__links__lang__content"]}>
+                        {t('opposite-locale')}
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
