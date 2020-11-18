fc6dbc89af7e41c57b74bb5f9165d06a71de3783
kjacques <kjacques@seedbox.com>
2020-11-18 13:19:52 -0500

    feat: #121 styling the dictionary

--- website/src/components/FileFilters/index.js
+++ website/src/components/FileFilters/index.js
@@ -19,19 +19,12 @@
  */
 
 import React from 'react';
-import Select from '@icgc-argo/uikit/form/Select';
-import Input from '@icgc-argo/uikit/form/Input';
+import Select from '../Select';
 import styles from './styles.module.css';
 import Typography from '@icgc-argo/uikit/Typography';
-import { styled } from '@icgc-argo/uikit';
-import debounce from 'lodash/debounce';
 
 export const NO_ACTIVE_FILTER = 'no_active_filter';
-export const DEFAULT_FILTER = [{ content: 'All', value: NO_ACTIVE_FILTER }];
-
-const StyledSelect = styled(Select)`
-  min-width: 190px;
-`;
+export const DEFAULT_FILTER = [{ label: 'All', value: NO_ACTIVE_FILTER }];
 
 const FileFilters = ({
   dataTiers = [],
@@ -40,38 +33,26 @@ const FileFilters = ({
   onSearch = e => console.log(e.target.val),
 }) => {
   // update search params
-  const onSelect = filterName => value => onSearch({ ...searchParams, ...{ [filterName]: value } });
-
-  const [inputValue, setInputValue] = React.useState('');
-  const applySearch = debounce(onSearch, 500);
+  const onSelect = filterName => ({value}) => onSearch({ ...searchParams, ...{ [filterName]: value } })
+console.log(searchParams.attribute)
   return (
     <Typography variant="data" color="#151c3d">
       <div className={styles.fileFilters}>
         <div className={styles.dataSelectors}>
           Data Tier:{' '}
-          <StyledSelect
+          <Select
+            className={styles.select}
             options={dataTiers}
             value={searchParams.tier}
             onChange={onSelect('tier')}
-            size="sm"
           />
           Attribute:{' '}
-          <StyledSelect
+          <Select
+            className={styles.select}
             options={dataAttributes}
             value={searchParams.attribute}
             onChange={onSelect('attribute')}
-            size="sm"
           />
-          {/*<Input
-            onChange={e => {
-              setInputValue(e.target.value);
-              applySearch(e.target.value);
-            }}
-            value={inputValue}
-            placeholder="Search Dictionary..."
-            preset="search"
-            className={styles.search}
-          />*/}
         </div>
       </div>
     </Typography>
