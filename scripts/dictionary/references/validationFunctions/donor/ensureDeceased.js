a838a0199c1db5ac0bcdd86e9eb9b7b60e2efaaa
Patrice Laplante <plaplante@ferlab.bio>
2021-04-15 16:12:40 -0400

    feat: #267 test validation script

--- scripts/dictionary/references/validationFunctions/donor/ensureDeceased.js
+++ scripts/dictionary/references/validationFunctions/donor/ensureDeceased.js
@@ -1,24 +1,25 @@
 /**
  * If and only if a donor is deceased, then the cause of death must be provided.
  */
-const validation = ($row, $field, $name) =>
-    (function validate() {
-        let result = {valid: true, message: 'Ok'};
-        const currField = typeof $field === 'string' ? $field.trim().toLowerCase() : $field;
-        const vitalStatus = $row.vital_status.trim().toLowerCase();
+const validation = () =>
+  function validate(inputs) {
+    const { $row, $field, $name } = inputs;
+    let result = { valid: true, message: 'Ok' };
+    const currField = typeof $field === 'string' ? $field.trim().toLowerCase() : $field;
+    const vitalStatus = $row.vital_status.trim().toLowerCase();
 
-        if (!currField && vitalStatus.toLowerCase() === 'deceased') {
-            result = {
-                valid: false,
-                message: `${$name} must be provided when the donor's vital_status is deceased.`,
-            };
-        } else if (currField && vitalStatus.toLowerCase() !== 'deceased') {
-            result = {
-                valid: false,
-                message: `${$name} cannot be provided if the donor's vital_status is not deceased.`,
-            };
-        }
-        return result;
-    })();
+    if (!currField && vitalStatus.toLowerCase() === 'deceased') {
+      result = {
+        valid: false,
+        message: `${$name} must be provided when the donor's vital_status is deceased.`,
+      };
+    } else if (currField && vitalStatus.toLowerCase() !== 'deceased') {
+      result = {
+        valid: false,
+        message: `${$name} cannot be provided if the donor's vital_status is not deceased.`,
+      };
+    }
+    return result;
+  };
 
 module.exports = validation;
