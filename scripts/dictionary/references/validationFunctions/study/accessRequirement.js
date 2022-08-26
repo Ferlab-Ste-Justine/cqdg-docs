b6e9979d3baa026c678e8553dc9ae32ed74756b9
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2022-08-26 11:19:52 -0400

    fix: CQDG-00 - fix dictionnary for 5.70 (#86)
    Co-authored-by: Aymeric Toulouse <atoulouse@caresimple.com>

--- scripts/dictionary/references/validationFunctions/study/accessRequirement.js
+++ scripts/dictionary/references/validationFunctions/study/accessRequirement.js
@@ -12,9 +12,13 @@ const validation = () =>
             "DUO:0000026",
             "DUO:0000027",
             "DUO:0000028",
-            "DUO:0000029"
+            "DUO:0000029",
+            "DUO:0000043",
+            "DUO:0000044",
+            "DUO:0000045",
+            "DUO:0000046"
           ];
-        
+
         const {$row, $name} = inputs;
         const $field = $row[$name];
 
@@ -33,11 +37,11 @@ const validation = () =>
 
         if (!valid) {
             result = {
-                valid: false, 
+                valid: false,
                 message: `${$name} must contain a list of these codes: ${list.join(';')} separated by semicolon (;)`
             }
         }
-        
+
         return result;
     });
 
